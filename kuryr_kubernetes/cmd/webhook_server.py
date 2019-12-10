d8532bb715d8bf6b0041c7c2dcdb5c48db384f13
Luis Tomas Bolivar <ltomasbo@redhat.com>
2019-12-10 18:15:19 +0100

    Ensure kuryr webhook is running before other operators' pods
    Change-Id: Id74eb4d8a7b1ea7ec97085de85f29244bbda25ea

--- kuryr_kubernetes/cmd/webhook_server.py
+++ kuryr_kubernetes/cmd/webhook_server.py
@@ -16,18 +16,53 @@
 import argparse
 import base64
 import copy
+import os
 
 from flask import Flask
 from flask import jsonify
 from flask import request
 import jsonpatch
 
+from kuryr_kubernetes import constants
+from kuryr_kubernetes import exceptions
+from kuryr_kubernetes import k8s_client
 
 app = Flask(__name__)
+first_request = True
 
 
 @app.route('/', methods=['POST'])
 def webhook():
+    global first_request
+    if first_request:
+        first_request = False
+        host = os.environ['KUBERNETES_SERVICE_HOST']
+        port = os.environ['KUBERNETES_SERVICE_PORT']
+        api_root = "https://%s:%s" % (host, port)
+        k8s = k8s_client.K8sClient(api_root)
+        # list of namespaces with operators to delete
+        for ns in app.config['ns_to_clear']:
+            try:
+                pods = k8s.get('{}/namespaces/{}/pods'.format(
+                    constants.K8S_API_BASE, ns))
+            except (exceptions.K8sClientException,
+                    exceptions.K8sResourceNotFound):
+                # unexpected exception, ensure it gets retried
+                first_request = True
+                continue
+            for pod in pods.get('items'):
+                options = pod['spec'].get('dnsConfig', {}).get('options', [])
+                if not [o for o in options if o['name'] == 'use-vc']:
+                    try:
+                        k8s.delete('{}/namespaces/{}/pods/{}'.format(
+                            constants.K8S_API_BASE, ns,
+                            pod['metadata']['name']))
+                    except (exceptions.K8sClientException,
+                            exceptions.K8sResourceNotFound):
+                        # unexpected exception, ensure it gets retried
+                        first_request = True
+                        continue
+
     request_info = request.json
 
     obj = request_info['request']['object']
@@ -78,8 +113,13 @@ def main():
         '--tls-private-key-file', default='key.pem',
         help='File containing the default x509 private key matching '
              '--tls-cert-file.')
+    parser.add_argument(
+        '--ns', action='append',
+        help='List of namespaces with pods to delete to ensure right creation '
+             'order.')
     args = parser.parse_args()
 
+    app.config['ns_to_clear'] = args.ns
     app.run(host=args.bind_address, port=args.port,
             ssl_context=(args.tls_cert_file, args.tls_private_key_file))
 
