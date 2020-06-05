610a9d1de610d631e9c909a37be8951169dab36c
Michał Dulko <mdulko@redhat.com>
2020-06-05 10:46:44 +0200

    Get RPM and Kuryr version from git
    This commit makes sure we're getting the version to use as RPM and
    Python package semver from the last tag in git. This should solve issue
    of `kuryr-k8s-controller --version` returning 0.0.0 and remove the need
    to update versions in various places each time master gets opened to new
    release development.
    
    Change-Id: I5f2d56e1cdad6dace4c456ee7a6ff34f57a02b5d

--- tools/build-rpm-rhel8.sh
+++ tools/build-rpm-rhel8.sh
@@ -1,14 +1,19 @@
 #!/bin/bash -x
 
-version=4.5.1
 source_path=_output/SOURCES
 
 mkdir -p ${source_path}
 
+# Getting version from last tag in git
+version=`git describe --tags --abbrev=0`
+version=${version#openshift-kuryr-} # remove prefix
+release=${version#*-} # remove prefix
+version=${version%-*} # remove date suffix
+
 # NOTE(dulek): rpmbuild requires that inside the tar there will be a
 #              ${service}-${version} directory, hence this --transform option.
-#              Also note that for some reason it doesn't work without excluding
-#              .git directory. Excluding .tox is convenient for local builds.
+#              We exclude .git as rpmbuild will do its own `git init`.
+#              Excluding .tox is convenient for local builds.
 tar -czvf ${source_path}/kuryr-kubernetes.tar.gz --exclude=.git --exclude=.tox --transform "flags=r;s|\.|kuryr-kubernetes-${version}|" .
 cp kuryr.logrotate ${source_path}
 cp kuryr-controller.service ${source_path}
@@ -20,5 +25,5 @@ curl http://base-openstack-4-3.ocp.svc > /etc/yum.repos.d/base-openstack-4-3.rep
 
 yum install -y python3-pbr python3-devel
 
-rpmbuild -ba -D "_topdir `pwd`/_output" openshift-kuryr-kubernetes-rhel8.spec
+rpmbuild -ba -D "_version $version" -D "_release $release" -D "_topdir `pwd`/_output" openshift-kuryr-kubernetes-rhel8.spec
 createrepo _output/RPMS/noarch
