b056952ceb443a6194d1d41c02f8f681440b0232
Michał Dulko <mdulko@redhat.com>
2020-06-05 10:46:44 +0200

    Test RPM version
    Change-Id: I5f2d56e1cdad6dace4c456ee7a6ff34f57a02b5d

--- tools/build-rpm-rhel8.sh
+++ tools/build-rpm-rhel8.sh
@@ -9,7 +9,10 @@ mkdir -p ${source_path}
 #              ${service}-${version} directory, hence this --transform option.
 #              Also note that for some reason it doesn't work without excluding
 #              .git directory. Excluding .tox is convenient for local builds.
-tar -czvf ${source_path}/kuryr-kubernetes.tar.gz --exclude=.git --exclude=.tox --transform "flags=r;s|\.|kuryr-kubernetes-${version}|" .
+env
+git tag
+git tag $version
+tar -czvf ${source_path}/kuryr-kubernetes.tar.gz --exclude=.tox --transform "flags=r;s|\.|kuryr-kubernetes-${version}|" .
 cp kuryr.logrotate ${source_path}
 cp kuryr-controller.service ${source_path}
 cp openshift-kuryr.tmpfs ${source_path}
